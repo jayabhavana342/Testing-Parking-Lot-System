4f0e91a6284487f5d4bd34fe1514e6b93f25429c
Bhavana Kommuru <jayanagabhavanakommuru@gmail.com>
2018-02-24 20:36:41 -0600

    created table for users

--- MySQL data/parking_lot.sql
+++ MySQL data/parking_lot.sql
@@ -8,3 +8,14 @@ USE parkinglot;
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS frequent_parking_users;
 DROP TABLE IF EXISTS parking_history;
+
+# Table user's
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `last_name` varchar(64) DEFAULT NULL,
+  `first_name` varchar(64) DEFAULT NULL,
+  `email` varchar(64) DEFAULT NULL,
+  `password` varchar(256) DEFAULT NULL,
+  `is_admin` tinyint DEFAULT 0,
+  PRIMARY KEY (`id`)
+) AUTO_INCREMENT=1;
