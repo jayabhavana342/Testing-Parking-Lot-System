39f96675908f5306876a0249edbaa793c3409f2e
Bhavana Kommuru <jayanagabhavanakommuru@gmail.com>
2018-02-24 20:41:09 -0600

    created frequent parking users table

--- MySQL data/parking_lot.sql
+++ MySQL data/parking_lot.sql
@@ -9,17 +9,31 @@ DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS frequent_parking_users;
 DROP TABLE IF EXISTS parking_history;
 
-# Table user's
+# User's Table
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `last_name` varchar(64) DEFAULT NULL,
-  `first_name` varchar(64) DEFAULT NULL,
-  `email` varchar(64) DEFAULT NULL,
-  `password` varchar(256) DEFAULT NULL,
-  `is_admin` tinyint DEFAULT 0,
+  `id`         INT(11) NOT NULL AUTO_INCREMENT,
+  `last_name`  VARCHAR(64)      DEFAULT NULL,
+  `first_name` VARCHAR(64)      DEFAULT NULL,
+  `email`      VARCHAR(64)      DEFAULT NULL,
+  `password`   VARCHAR(256)     DEFAULT NULL,
+  `is_admin`   TINYINT          DEFAULT 0,
   PRIMARY KEY (`id`)
-) AUTO_INCREMENT=1;
+)
+  AUTO_INCREMENT = 1;
 
 # Insert into table users
 INSERT INTO `users` (`id`, `last_name`, `first_name`, `email`, `password`, `is_admin`)
 VALUES (1, 'Alpha', 'Joe', 'joe.alpha@foo.com', 'admin', 0);
+
+# Frequent Parking User's table
+CREATE TABLE `frequent_parking_users` (
+  `id`         INT(11) NOT NULL AUTO_INCREMENT,
+  `last_name`  VARCHAR(64)      DEFAULT NULL,
+  `first_name` VARCHAR(64)      DEFAULT NULL,
+  `email`      VARCHAR(64)      DEFAULT NULL,
+  `address`    VARCHAR(100)     DEFAULT NULL,
+  `phone`      INT(10)          DEFAULT NULL,
+  `license_id` VARCHAR(10)      DEFAULT NULL,
+  PRIMARY KEY (`id`)
+)
+  AUTO_INCREMENT = 1;
