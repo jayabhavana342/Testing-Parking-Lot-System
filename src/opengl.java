0ba2e8313dcf49354c0e37b09a017a808ec5fb65
Bhavana Kommuru <jayanagabhavanakommuru@gmail.com>
2018-04-09 22:08:49 -0500

    jogl matrix points

--- src/opengl.java
+++ src/opengl.java
@@ -73,16 +73,13 @@ class OneTriangle {
 //        gl.glEnd();
 
 
-
         gl.glBegin(GL2.GL_LINES);
-        gl.glColor3f(1.0f,1.0f,1.0f); // White (RGB)
-        for( float x = 0; x < width; x += 1 )
-        {
+        gl.glColor3f(1.0f, 1.0f, 1.0f); // White (RGB)
+        for (float x = 0; x < width; x += 1) {
             gl.glVertex3f(x, 0.0f, 0.0f);
             gl.glVertex3f(x, (width), 0.0f);
         }
-        for( float y = 0; y < height; y += 1 )
-        {
+        for (float y = 0; y < height; y += 1) {
             gl.glVertex3f(0.0f, y, 0.0f);
             gl.glVertex3f(width, y, 0.0f);
         }
@@ -122,65 +119,74 @@ class OneTriangleAWT {
             public void display(GLAutoDrawable glautodrawable) {
                 OneTriangle.render(glautodrawable.getGL().getGL2(), glautodrawable.getSurfaceWidth(), glautodrawable.getSurfaceHeight());
 
-//                final GL2 gl = glautodrawable.getGL().getGL2();
-//                gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
-//                gl.glEnable(GL2.GL_TEXTURE_2D);
-//                int textureId = 0;
-//                gl.glBindTexture(GL2.GL_TEXTURE_2D, textureId);
-//                gl.glPushMatrix();
-//                gl.glOrtho(0, 1, 0, 1, 0, 1);
+                final GL2 gl = glautodrawable.getGL().getGL2();
+                gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
+                gl.glEnable(GL2.GL_TEXTURE_2D);
+                int textureId = 0;
+                gl.glBindTexture(GL2.GL_TEXTURE_2D, textureId);
+                gl.glPushMatrix();
+                gl.glOrtho(0, 10, 0, 10, 0, 10);
 
-//                gl.glMatrixMode(GL2.GL_MODELVIEW);
-//                gl.glMatrixMode(GL2.GL_PROJECTION);
+                gl.glMatrixMode(GL2.GL_MODELVIEW);
+                gl.glMatrixMode(GL2.GL_PROJECTION);
+                gl.glPushMatrix();
+                gl.glLoadIdentity();
+                gl.glDepthMask(false);
+
+                try {
+                    File im2 = new File("F:\\jayabhavana342\\Parking-Lot-System\\images\\layout.png");
+                    Texture t2 = TextureIO.newTexture(im2, true);
+                    textureId = t2.getTextureObject(gl);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                gl.glBegin(GL2.GL_QUADS);
+                gl.glTexCoord2f(0f, 0f);
+                gl.glVertex2f(0, 0);
+                gl.glTexCoord2f(0f, 1f);
+                gl.glVertex2f(0, 10);
+                gl.glTexCoord2f(1f, 1f);
+                gl.glVertex2f(10, 10);
+                gl.glTexCoord2f(1f, 0f);
+                gl.glVertex2f(10, 0);
+                gl.glEnd();
+
+                gl.glDepthMask(true);
+                gl.glPopMatrix();
+
 //                gl.glPushMatrix();
 //                gl.glLoadIdentity();
-//                gl.glDepthMask(false);
-
-//                try {
-//                    File im2 = new File("F:\\jayabhavana342\\Parking-Lot-System\\images\\layout.png");
-//                    Texture t2 = TextureIO.newTexture(im2, true);
-//                    textureId = t2.getTextureObject(gl);
-//                } catch (IOException e) {
-//                    e.printStackTrace();
+//                gl.glBegin(GL2.GL_POINTS);
+//                for (float i = 0; i <= 10; ) {
+//                    gl.glVertex2f(i, i);
+//                    i += 0.01;
 //                }
-//
-//                gl.glBegin(GL2.GL_QUADS);
-//                gl.glTexCoord2f(0f, 0f);
-//                gl.glVertex2f(0, 0);
-//                gl.glTexCoord2f(0f, 1f);
-//                gl.glVertex2f(0, 1);
-//                gl.glTexCoord2f(1f, 1f);
-//                gl.glVertex2f(1, 1);
-//                gl.glTexCoord2f(1f, 0f);
-//                gl.glVertex2f(1, 0);
 //                gl.glEnd();
-
-//                gl.glColor3f(3,3,3);
-//                gl.glBegin(GL2.GL_QUADS);
-//                gl.glVertex3f( 0, (float) -0.001, 0);
-//                gl.glVertex3f( 0, (float) -0.001,10);
-//                gl.glVertex3f(10, (float) -0.001,10);
-//                gl.glVertex3f(10, (float) -0.001, 0);
-//                gl.glEnd();
-//
-//                gl.glBegin(GL_LINES);
-//                for(int i=0;i<=10;i++) {
-//                    if (i==0) { gl.glColor3f(6,3,3); } else { gl.glColor3f(25,25,25); };
-//                    gl.glVertex3f(i,0,0);
-//                    gl.glVertex3f(i,0,10);
-//                    if (i==0) { gl.glColor3f(3,3,6); } else { gl.glColor3f(25,25,25); };
-//                    gl.glVertex3f(0,0,i);
-//                    gl.glVertex3f(10,0,i);
-//                };
-//                gl.glEnd();
-//
-//                gl.glDepthMask(true);
 //                gl.glPopMatrix();
+
+                gl.glPushMatrix();
+                gl.glLoadIdentity();
+                gl.glPointSize( 6.0f);
+                gl.glColor3f(0.0f,1.0f,0.0f);
+                gl.glBegin(GL2.GL_POINTS);
+                for (float i = 0; i < 10; ) {
+                    for (float j = 1; j < 10;) {
+                        gl.glVertex2f(i, j);
+                        System.out.print("(" + i + " : " + j + ") ");
+                        j = j + (float) 0.2;
+                    }
+                    System.out.println();
+                    i = i + (float) 0.2;
+                }
+                gl.glEnd();
+                gl.glPopMatrix();
+
 
             }
         });
 
-        glcanvas.setSize(600, 600);
+        glcanvas.setSize(600, 800);
 
         final Frame frame = new Frame("One Triangle AWT");
         frame.add(glcanvas);
@@ -192,7 +198,7 @@ class OneTriangleAWT {
             }
         });
 
-        frame.setSize(640, 480);
+        frame.setSize(1240, 880);
         frame.setVisible(true);
     }
 }
